5d12df58d98cffe6232a410fb54b4108c6352de4
Kim Wahlman <kim.wahlman@gmail.com>
2018-10-15 23:38:08 +0200

    Update rotLeft.cpp

--- Source Files/rotLeft.cpp
+++ Source Files/rotLeft.cpp
@@ -7,3 +7,23 @@ std::vector<int> rotLeft(std::vector<int> &arr, const int d)
     std::rotate(arr.begin(), arr.begin() + d, arr.end());
     return arr;
 }
+
+/* 
+// This code do what the challenge ask it to do, but it is to slow to solve this particular challenge.
+// O(n^2) complexity if I ain't mistaken. 
+
+std::vector<int> rotLeft(std::vector<int> &arr, const int d) 
+{
+    for(int i = 0; i < d; i++)
+    {
+        for(int j = 0; j < arr.size(); j++)
+        {
+            int temp = arr[j];
+            arr[j] = arr[j+1]
+            arr[j+1] = temp;
+        }
+    } 
+    return arr;
+}
+
+*/
