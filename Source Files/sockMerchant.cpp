26fade852cd405714d026cd90fe593c9a0a848da
Kim Wahlman <kim.wahlman@gmail.com>
2018-10-16 02:39:07 +0200

    Update sockMerchant.cpp
    Fixed a typo on row 9.  
    if(temp[i] > 2)  { ... } became if(temp[i] >= 2) { ... }

--- Source Files/sockMerchant.cpp
+++ Source Files/sockMerchant.cpp
@@ -6,7 +6,7 @@ int sockMerchant(int n, std::vector<int> &arr)
     for(int i = 0; i < arr.size(); i++)
         temp[arr[i]]++; // Take value from index arr[i] and increment by 1.
     for(int i = 0; i != 100; i++)
-        if(temp[i] > 2) // No need to do the res += ... operation on values lower than 2
+        if(temp[i] >= 2) // No need to do the res += ... operation on values lower than 2
             res += temp[i] / 2; // Divide the value in temp[i] by two to find the number of pairs of that color.
 
     return res;
